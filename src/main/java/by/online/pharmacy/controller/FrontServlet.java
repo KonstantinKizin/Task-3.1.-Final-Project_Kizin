2e8752b08a07ec88bcd9ac69330875bcb97e2423
Kosty Kizin <KostyKizin@gmail.com>
2017-11-21 10:13:56 +0300

    X

--- src/main/java/by/online/pharmacy/controller/FrontServlet.java
+++ src/main/java/by/online/pharmacy/controller/FrontServlet.java
@@ -11,7 +11,6 @@ import org.apache.log4j.Logger;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,8 +19,6 @@ import java.io.IOException;
 import java.util.Map;
 import static by.online.pharmacy.dao.impl.PropertyManager.getProperty;
 
-
-@WebServlet(name = "FrontServlet")
 public class FrontServlet extends HttpServlet {
 
     private final ServiceFactory factory = ServiceFactory.getInstance();
