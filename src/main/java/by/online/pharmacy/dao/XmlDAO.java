e075efbb1caee2f3cb51a046061c48e4bd0147da
Kosty Kizin <KostyKizin@gmail.com>
2017-12-13 13:17:19 +0300

    s

--- src/main/java/by/online/pharmacy/dao/XmlDAO.java
+++ src/main/java/by/online/pharmacy/dao/XmlDAO.java
@@ -2,11 +2,7 @@ package by.online.pharmacy.dao;
 
 import by.online.pharmacy.dao.exception.DAOException;
 import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-
-import java.util.Map;
 
 public interface XmlDAO {
     Document getDocument() throws DAOException;
-    //Map<String , String> getCommandMap(NodeList nodeList);
 }
